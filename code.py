755fbe64574d796f91d8c72c0d6c3a1f01cb4ffe
jagrit2008 <70403360+jagrit2008@users.noreply.github.com>
2021-08-16 14:14:43 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,30 @@
+import plotly.figure_factory as ff
+import plotly.graph_objects as go
+import statistics
+import random
+import pandas as pd
+import csv
+
+df = pd.read_csv("medium_data.csv")
+data = df["population"].tolist()
+
+def random_set_of_mean(counter):
+    dataset = []
+    for i in range(0, counter):
+        random_index= random.randint(0,len(data)-1)
+        value = data[random_index]
+        dataset.append(value)
+    mean = statistics.mean(dataset)
+    return mean
+
+    def setup():
+        mean_list = []
+    for i in range(0,1000):
+        set_of_means= random_set_of_mean(100)
+        mean_list.append(set_of_means)
+    show_fig(mean_list)
+
+def show_fig(mean_list):
+   df=mean_list
+   fig=ff.create_distplot([df],["population"],show_hist=False)  
+   fig.show
